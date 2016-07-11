307126144ed96a0b72fe746c30186bb49f70dfe7
Adrian Kulinski (PGS Software) <akulinski@pgs-soft.com>
2016-07-11 08:35:45 +0200

    Init Commit

---
+++ LoyaltyProgram/src/main/java/com/pgs/soft/LoyaltyProgramApplication.java
@@ -0,0 +1,12 @@
+package com.pgs.soft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoyaltyProgramApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoyaltyProgramApplication.class, args);
+	}
+}
