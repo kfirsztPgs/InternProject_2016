b065898fa4b8dd3562e50ecd9ee25205510c224e
Adrian Kulinski (PGS Software) <akulinski@pgs-soft.com>
2016-07-11 13:17:50 +0200

    regex email validation

--- LoyaltyProgram/src/main/java/com/pgs/soft/UserDtoValidator.java
+++ LoyaltyProgram/src/main/java/com/pgs/soft/UserDtoValidator.java
@@ -1,5 +1,8 @@
 package com.pgs.soft;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.validation.Errors;
@@ -32,20 +35,25 @@ public class UserDtoValidator implements Validator {
 	
 	private void validatePassword(Errors errors, UserDTO userDTO){
 		if(!userDTO.getPassword().equals(userDTO.getPasswordRepeated())){
-			errors.reject("passwords.nomatch", "Hasla nie pasuja");
+			errors.reject("passwords.nomatch", "Passwords does not match");
 		}
 	}
 	
 	private void validateEmail(Errors errors, UserDTO userDTO) {
         if (userService.getUserByEmail(userDTO.getEmail()).isPresent()) {
-            errors.reject("email.exists", "Istnieje taki");
+            errors.reject("email.exists", "User with that email already exists");
         }
         if (!isEmailFormCorrect(userDTO.getEmail())){
-        	errors.reject("email.incorrect", "To nie email");
+        	errors.reject("email.incorrect", "Incorrect email");
         } 
     }
 	
 	private boolean isEmailFormCorrect(String email){
-		return true;
+		Pattern pattern;
+		Matcher matcher;
+		String emailPattern = "[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,4}";
+		pattern = Pattern.compile(emailPattern);
+		matcher = pattern.matcher(email);
+		return matcher.matches();
 	}
 }
